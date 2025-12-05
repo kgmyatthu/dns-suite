587593bab609559ea007cbcddd4d7a122eeda315
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2025-12-05 10:42:30 +0700

    Move dns_core tests into modules

--- dns_core/src/question.rs
+++ dns_core/src/question.rs
@@ -32,3 +32,24 @@ impl DnsQuestion {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::DnsQuestion;
+    use crate::{buffer::BytePacketBuffer, types::QueryType};
+
+    #[test]
+    fn question_write_and_read_roundtrip() {
+        let question = DnsQuestion::new("rust-lang.org".into(), QueryType::MX);
+        let mut buffer = BytePacketBuffer::new();
+        question.write(&mut buffer).unwrap();
+        let written_position = buffer.pos();
+
+        buffer.seek(0);
+        let mut parsed = DnsQuestion::new(String::new(), QueryType::UNKNOWN(0));
+        parsed.read(&mut buffer).unwrap();
+
+        assert_eq!(question, parsed);
+        assert_eq!(written_position, buffer.pos());
+    }
+}
--- dns_core/src/types.rs
+++ dns_core/src/types.rs
@@ -238,3 +238,26 @@ impl ResultCode {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{QueryType, ResultCode};
+
+    #[test]
+    fn query_type_to_and_from_num_are_inverse_for_known_type() {
+        let qtype = QueryType::MX;
+        assert_eq!(qtype, QueryType::from_num(qtype.to_num()));
+    }
+
+    #[test]
+    fn unknown_query_type_preserves_value() {
+        let unknown_value = 9999;
+        assert_eq!(QueryType::UNKNOWN(unknown_value), QueryType::from_num(unknown_value));
+        assert_eq!(unknown_value, QueryType::UNKNOWN(unknown_value).to_num());
+    }
+
+    #[test]
+    fn result_code_from_num_handles_known_values() {
+        assert_eq!(ResultCode::REFUSED, ResultCode::from_num(ResultCode::REFUSED as u8));
+    }
+}
